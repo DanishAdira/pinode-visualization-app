593b278d8b68fb37af7a895cc71e3cbd67449cea
DanishAdira <DanishAdira@danish.adira.0330@gmail.com>
2025-08-19 19:00:47 +0900

    [update] index.tsx修正

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,33 @@ import reportWebVitals from './reportWebVitals';
 import './index.css';
 
 import { Amplify } from 'aws-amplify';
-import awsExports from './aws-exports';
-Amplify.configure(awsExports);
+// aws-exports.jsの直接インポートを削除
+
+// 環境変数からAmplify設定オブジェクトを構築
+const amplifyConfig = {
+  Auth: {
+    Cognito: {
+      userPoolId: process.env.REACT_APP_USER_POOLS_ID,
+      userPoolClientId: process.env.REACT_APP_USER_POOLS_WEB_CLIENT_ID,
+      identityPoolId: process.env.REACT_APP_COGNITO_IDENTITY_POOL_ID,
+    }
+  },
+  API: {
+    GraphQL: {
+      endpoint: process.env.REACT_APP_APPSYNC_GRAPHQLENDPOINT,
+      region: process.env.REACT_APP_AWS_PROJECT_REGION, // AppSyncリージョン
+      defaultAuthMode: 'iam' // AppSyncの認証タイプに合わせて変更
+    }
+  },
+  Storage: {
+    S3: {
+      bucket: process.env.REACT_APP_STORAGE_BUCKET,
+      region: process.env.REACT_APP_AWS_PROJECT_REGION, // S3リージョン
+    }
+  }
+};
+
+Amplify.configure(amplifyConfig);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
